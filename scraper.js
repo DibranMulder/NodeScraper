46420bd3fd346e806778503b5528da137d3cda1f
Dibran Mulder <superdib88@hotmail.com>
2018-11-26 22:52:44 +0100

    Fixed debugging and azure storage integration

--- scraper.js
+++ scraper.js
@@ -37,166 +37,241 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var puppeteer = require("puppeteer");
+var azure = require("azure-storage");
+var uuidv4 = require('uuid/v4');
 var timeout = 1000;
-var eanCodes = ['8711252416793', '2950001557127'];
-puppeteer.launch().then(function (browser) { return __awaiter(_this, void 0, void 0, function () {
-    var _i, eanCodes_1, ean, page, searchBtn, url, buyBtn, modalClose, titleElement, title, priceElement, price, basketBtn, url, maxVal, okBtn, availableQ, removeBtn, error_1;
+var eanCodes = [];
+var options = {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    javascriptEnabled: true,
+    phantomConfig: { "ssl-protocol": "ANY", 'ignore-ssl-errors': 'true' }
+};
+// Create table service.
+var tableService = azure.createTableService('bolcomdata', 'kI5r3eynZTg92sosrgBg/lx8rmww2xI9CaJzxtXYU938DGAP4bEqkAesY9q1ZhAscRWdwL1EoHBjzdiV6czJcQ==');
+var entGen = azure.TableUtilities.entityGenerator;
+puppeteer.launch(options).then(function (browser) { return __awaiter(_this, void 0, void 0, function () {
     return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 34, , 35]);
-                _i = 0, eanCodes_1 = eanCodes;
-                _a.label = 1;
-            case 1:
-                if (!(_i < eanCodes_1.length)) return [3 /*break*/, 33];
-                ean = eanCodes_1[_i];
-                return [4 /*yield*/, browser.newPage()];
-            case 2:
-                page = _a.sent();
-                return [4 /*yield*/, page.goto('https://www.bol.com/nl/')];
-            case 3:
-                _a.sent();
-                // Search for an EAN code.
-                console.log("Searching for: " + ean);
-                return [4 /*yield*/, page.focus('#searchfor')];
-            case 4:
-                _a.sent();
-                return [4 /*yield*/, page.keyboard.type(ean)];
-            case 5:
-                _a.sent();
-                return [4 /*yield*/, page.$("[data-test='search-button']")];
-            case 6:
-                searchBtn = _a.sent();
-                return [4 /*yield*/, searchBtn.click()];
-            case 7:
-                _a.sent();
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 8:
-                _a.sent();
-                url = page.url();
-                console.log("Page at: " + url);
-                return [4 /*yield*/, page.$(".btn--buy")];
-            case 9:
-                buyBtn = _a.sent();
-                if (buyBtn) {
-                    console.log("Product in basket.");
-                    buyBtn.click();
-                }
-                else {
-                    console.log("Product not added to basket.");
-                    return [3 /*break*/, 32];
-                }
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 10:
-                _a.sent();
-                return [4 /*yield*/, page.$("[data-test='modal-window-close']")];
-            case 11:
-                modalClose = _a.sent();
-                modalClose.click();
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 12:
-                _a.sent();
-                return [4 /*yield*/, page.$("[data-test='title']")];
-            case 13:
-                titleElement = _a.sent();
-                return [4 /*yield*/, page.evaluate(function (el) { return el.innerText; }, titleElement)];
-            case 14:
-                title = _a.sent();
-                console.log("Title: " + title);
-                return [4 /*yield*/, page.$("[data-test='price']")];
-            case 15:
-                priceElement = _a.sent();
-                return [4 /*yield*/, page.evaluate(function (el) { return el.innerText; }, priceElement)];
-            case 16:
-                price = _a.sent();
-                console.log("Price: " + parseFloat(price));
-                return [4 /*yield*/, page.$("[data-test='basket-button']")];
-            case 17:
-                basketBtn = _a.sent();
-                basketBtn.click();
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 18:
-                _a.sent();
-                url = page.url();
-                console.log("Page at: " + url);
-                console.log("At basket");
-                return [4 /*yield*/, page.evaluate(function () {
-                        var m = 0;
-                        var select = document.querySelector('#tst_quantity_dropdown');
-                        for (var i = 0; i < select.options.length; i++) {
-                            if (select.options[i].value === "meer") {
-                                return undefined;
-                            }
-                            var intVal = parseInt(select.options[i].value);
-                            if (intVal > m) {
-                                m = intVal;
-                            }
+        try {
+            tableService.queryEntities('eancodes', null, null, function (error, result, response) {
+                return __awaiter(this, void 0, void 0, function () {
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                if (!error) {
+                                    console.log("Entires there");
+                                    result.entries.forEach(function (element) {
+                                        eanCodes.push(element["EanCode"]._);
+                                    });
+                                }
+                                else {
+                                    console.log(error);
+                                }
+                                return [4 /*yield*/, scrapeItems(browser)];
+                            case 1:
+                                _a.sent();
+                                console.log("Done.");
+                                return [2 /*return*/];
                         }
-                        return m;
-                    })];
-            case 19:
-                maxVal = _a.sent();
-                if (!(maxVal == undefined)) return [3 /*break*/, 27];
-                console.log("Max val is undefined");
-                return [4 /*yield*/, page.select('#tst_quantity_dropdown', 'meer')];
-            case 20:
-                _a.sent();
-                console.log("Select more");
-                return [4 /*yield*/, page.evaluate(function () {
-                        var element = document.querySelector('input[type=tel]');
-                        element.value = '';
-                        element.focus();
-                    })];
-            case 21:
-                _a.sent();
-                return [4 /*yield*/, page.type('input[type=tel]', '500')];
-            case 22:
-                _a.sent();
-                console.log("Set q to 500");
-                return [4 /*yield*/, page.$('.js_quantity_overlay_ok')];
-            case 23:
-                okBtn = _a.sent();
-                return [4 /*yield*/, okBtn.click()];
-            case 24:
-                _a.sent();
-                console.log("Click ok");
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 25:
-                _a.sent();
-                return [4 /*yield*/, page.evaluate(function () {
-                        var e = document.querySelector('#tst_quantity_dropdown');
-                        return e.options[e.selectedIndex].value;
-                    })];
-            case 26:
-                availableQ = _a.sent();
-                console.log("Max value: " + availableQ);
-                return [3 /*break*/, 28];
-            case 27:
-                console.log("Max value: " + maxVal);
-                _a.label = 28;
-            case 28: return [4 /*yield*/, page.$('#tst_remove_from_basket')];
-            case 29:
-                removeBtn = _a.sent();
-                return [4 /*yield*/, removeBtn.click()];
-            case 30:
-                _a.sent();
-                return [4 /*yield*/, page.waitFor(timeout)];
-            case 31:
-                _a.sent();
-                page.close();
-                _a.label = 32;
-            case 32:
-                _i++;
-                return [3 /*break*/, 1];
-            case 33:
-                console.log("Done.");
-                return [3 /*break*/, 35];
-            case 34:
-                error_1 = _a.sent();
-                console.log("Error");
-                console.error(error_1);
-                return [3 /*break*/, 35];
-            case 35: return [2 /*return*/];
+                    });
+                });
+            });
         }
+        catch (error) {
+            console.log("Error");
+            console.error(error);
+        }
+        return [2 /*return*/];
     });
 }); });
+function scrapeItems(browser) {
+    return __awaiter(this, void 0, void 0, function () {
+        var _i, eanCodes_1, ean, pot, page, searchBtn, url, titleElement, title, priceElement, price, floatPrice, buyBtn, modalClose, basketBtn, url, maxVal, okBtn, availableQ, removeBtn;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _i = 0, eanCodes_1 = eanCodes;
+                    _a.label = 1;
+                case 1:
+                    if (!(_i < eanCodes_1.length)) return [3 /*break*/, 33];
+                    ean = eanCodes_1[_i];
+                    pot = {
+                        PartitionKey: entGen.String(ean),
+                        RowKey: entGen.String(uuidv4()),
+                        Date: entGen.DateTime(new Date()),
+                        Ean: entGen.String(ean),
+                        Success: entGen.Boolean(false),
+                        Title: entGen.String(null),
+                        Price: entGen.Double(null),
+                        Quantity: entGen.Int32(null)
+                    };
+                    return [4 /*yield*/, browser.newPage()];
+                case 2:
+                    page = _a.sent();
+                    return [4 /*yield*/, page.goto('https://www.bol.com/nl/')];
+                case 3:
+                    _a.sent();
+                    // Search for an EAN code.
+                    console.log("Searching for: " + ean);
+                    return [4 /*yield*/, page.focus('#searchfor')];
+                case 4:
+                    _a.sent();
+                    return [4 /*yield*/, page.keyboard.type(ean)];
+                case 5:
+                    _a.sent();
+                    return [4 /*yield*/, page.$("[data-test='search-button']")];
+                case 6:
+                    searchBtn = _a.sent();
+                    return [4 /*yield*/, searchBtn.click()];
+                case 7:
+                    _a.sent();
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 8:
+                    _a.sent();
+                    url = page.url();
+                    console.log("Page at: " + url);
+                    return [4 /*yield*/, page.$("[data-test='title']")];
+                case 9:
+                    titleElement = _a.sent();
+                    return [4 /*yield*/, page.evaluate(function (el) { return el.innerText; }, titleElement)];
+                case 10:
+                    title = _a.sent();
+                    pot.Title = entGen.String(title);
+                    console.log("Title: " + title);
+                    return [4 /*yield*/, page.$("[data-test='price']")];
+                case 11:
+                    priceElement = _a.sent();
+                    return [4 /*yield*/, page.evaluate(function (el) { return el.innerText; }, priceElement)];
+                case 12:
+                    price = _a.sent();
+                    floatPrice = parseFloat(price.replace(/\n/g, '').replace(',', '.'));
+                    pot.Price = entGen.Double(floatPrice);
+                    console.log("Price: " + floatPrice);
+                    return [4 /*yield*/, page.$(".btn--buy")];
+                case 13:
+                    buyBtn = _a.sent();
+                    if (buyBtn) {
+                        console.log("Product in basket.");
+                        buyBtn.click();
+                    }
+                    else {
+                        console.log("Product not added to basket.");
+                        pot.Success = entGen.Boolean(false);
+                        pot.Quantity = entGen.Int32(0);
+                        insertRecord(pot);
+                        return [2 /*return*/];
+                    }
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 14:
+                    _a.sent();
+                    return [4 /*yield*/, page.$("[data-test='modal-window-close']")];
+                case 15:
+                    modalClose = _a.sent();
+                    modalClose.click();
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 16:
+                    _a.sent();
+                    return [4 /*yield*/, page.$("[data-test='basket-button']")];
+                case 17:
+                    basketBtn = _a.sent();
+                    basketBtn.click();
+                    console.log("At basket");
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 18:
+                    _a.sent();
+                    url = page.url();
+                    console.log("Page at: " + url);
+                    return [4 /*yield*/, page.evaluate(function () {
+                            var m = 0;
+                            var select = document.querySelector('#tst_quantity_dropdown');
+                            for (var i = 0; i < select.options.length; i++) {
+                                if (select.options[i].value === "meer") {
+                                    return undefined;
+                                }
+                                var intVal = parseInt(select.options[i].value);
+                                if (intVal > m) {
+                                    m = intVal;
+                                }
+                            }
+                            return m;
+                        })];
+                case 19:
+                    maxVal = _a.sent();
+                    if (!(maxVal == undefined)) return [3 /*break*/, 27];
+                    console.log("Max val is undefined");
+                    // Select meer.
+                    return [4 /*yield*/, page.select('#tst_quantity_dropdown', 'meer')];
+                case 20:
+                    // Select meer.
+                    _a.sent();
+                    console.log("Select more");
+                    return [4 /*yield*/, page.evaluate(function () {
+                            var element = document.querySelector('input[type=tel]');
+                            element.value = '';
+                            element.focus();
+                        })];
+                case 21:
+                    _a.sent();
+                    // Enter 500
+                    return [4 /*yield*/, page.type('input[type=tel]', '500')];
+                case 22:
+                    // Enter 500
+                    _a.sent();
+                    console.log("Set q to 500");
+                    return [4 /*yield*/, page.$('.js_quantity_overlay_ok')];
+                case 23:
+                    okBtn = _a.sent();
+                    return [4 /*yield*/, okBtn.click()];
+                case 24:
+                    _a.sent();
+                    console.log("Click ok");
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 25:
+                    _a.sent();
+                    return [4 /*yield*/, page.evaluate(function () {
+                            var e = document.querySelector('#tst_quantity_dropdown');
+                            return e.options[e.selectedIndex].value;
+                        })];
+                case 26:
+                    availableQ = _a.sent();
+                    console.log("Max value: " + availableQ);
+                    pot.Quantity = entGen.Int32(parseInt(availableQ));
+                    return [3 /*break*/, 28];
+                case 27:
+                    console.log("Max value: " + maxVal);
+                    pot.Quantity = entGen.Int32(maxVal);
+                    _a.label = 28;
+                case 28: return [4 /*yield*/, page.$('#tst_remove_from_basket')];
+                case 29:
+                    removeBtn = _a.sent();
+                    return [4 /*yield*/, removeBtn.click()];
+                case 30:
+                    _a.sent();
+                    return [4 /*yield*/, page.waitFor(timeout)];
+                case 31:
+                    _a.sent();
+                    // Close page.
+                    page.close();
+                    pot.Success = entGen.Boolean(true);
+                    insertRecord(pot);
+                    _a.label = 32;
+                case 32:
+                    _i++;
+                    return [3 /*break*/, 1];
+                case 33: return [2 /*return*/];
+            }
+        });
+    });
+}
+function insertRecord(pot) {
+    tableService.insertEntity('pointsintime', pot, function (error) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                if (!error) {
+                    console.log("Entity inserted");
+                }
+                return [2 /*return*/];
+            });
+        });
+    });
+}
+//# sourceMappingURL=scraper.js.map
--- scraper.ts
+++ scraper.ts
@@ -1,141 +1,202 @@
 import puppeteer = require('puppeteer');
+import azure = require('azure-storage');
+const uuidv4 = require('uuid/v4');
 
 const timeout = 1000;
-const eanCodes = ['8711252416793', '2950001557127'];
+const eanCodes = [];
 
-puppeteer.launch().then(async browser => {
+var options = {
+  args: ['--no-sandbox', '--disable-setuid-sandbox'], //without these settings puppeteer won't start
+  javascriptEnabled: true,
+  phantomConfig: { "ssl-protocol": "ANY", 'ignore-ssl-errors': 'true' }
+};
+
+// Create table service.
+let tableService = azure.createTableService('bolcomdata', '');
+var entGen = azure.TableUtilities.entityGenerator;
+
+puppeteer.launch(options).then(async browser => {
   try {
-    // Loop every ean code.
-    for (let ean of eanCodes) {
-      // Open a new page.
-      const page = await browser.newPage();
-      await page.goto('https://www.bol.com/nl/');
-      
-      // Search for an EAN code.
-      console.log("Searching for: " + ean);
-      await page.focus('#searchfor');
-      await page.keyboard.type(ean);
+    tableService.queryEntities('eancodes', null, null, async function (error, result, response) {
+      if (!error) {
+        console.log("Entires there");
 
-      var searchBtn = await page.$("[data-test='search-button']");
-      await searchBtn.click();
-
-      await page.waitFor(timeout);
-
-      // Log the current url.
-      var url = page.url();
-      console.log("Page at: " + url);
-
-      // Hit the buy btn.
-      var buyBtn = await page.$(".btn--buy");
-      if (buyBtn){
-        console.log("Product in basket.");
-        buyBtn.click();
+        result.entries.forEach(element => {
+          eanCodes.push(element["EanCode"]._);
+        });
       } else {
-        console.log("Product not added to basket.");
-        continue;
+        console.log(error);
       }
 
-      await page.waitFor(timeout);
+      await scrapeItems(browser);
 
-      // Close the modal.
-      var modalClose = await page.$("[data-test='modal-window-close']");
-      modalClose.click();
-      
-      await page.waitFor(timeout);
-
-      // Get the title
-      var titleElement = await page.$("[data-test='title']");
-      const title = await page.evaluate(el => el.innerText, titleElement);
-      console.log("Title: " + title);
-
-      // Get the price
-      var priceElement = await page.$("[data-test='price']");
-      const price = await page.evaluate(el => el.innerText, priceElement);
-      console.log("Price: " + parseFloat(price));
-
-      // Go to the basket.
-      var basketBtn = await page.$("[data-test='basket-button']");
-      basketBtn.click();
-      console.log("At basket");
-
-      await page.waitFor(timeout);
-
-      // Log the current url.
-      var url = page.url();
-      console.log("Page at: " + url);
-  
-      // Check for the highest number in the quantity select.
-      let maxVal: number | undefined = await page.evaluate(() => {
-        let m: number = 0;
-        
-        var select = document.querySelector('#tst_quantity_dropdown') as HTMLSelectElement;
-        for (var i = 0; i < select.options.length; i++) {
-          if (select.options[i].value === "meer") {
-            return undefined;
-          }
-  
-          let intVal = parseInt(select.options[i].value);
-          if (intVal > m){
-            m = intVal;
-          }
-        }
-        return m;
-      });
-      
-      // If the max value is undefined then we can select 'meer'.
-      if (maxVal == undefined) {
-        console.log("Max val is undefined");
-
-        // Select meer.
-        await page.select('#tst_quantity_dropdown', 'meer');
-  
-        console.log("Select more");
-
-        await page.evaluate(() => {
-          var element = document.querySelector('input[type=tel]') as HTMLInputElement;
-          element.value = '';
-          element.focus();
-        });
-
-        // Enter 500
-        await page.type('input[type=tel]', '500');
-
-        console.log("Set q to 500");
-  
-        // Press ok.
-        const okBtn = await page.$('.js_quantity_overlay_ok');
-        await okBtn.click();
-        
-        console.log("Click ok");
-
-        await page.waitFor(timeout);
-
-        // Query the available quantity.
-        var availableQ = await page.evaluate(() => {
-          var e = document.querySelector('#tst_quantity_dropdown') as HTMLSelectElement;
-          return e.options[e.selectedIndex].value;
-        });
-  
-        console.log("Max value: " + availableQ);
-  
-      } else {
-        console.log("Max value: " + maxVal);
-      }
-  
-      // Remove it from the basket.
-      const removeBtn = await page.$('#tst_remove_from_basket');
-      await removeBtn.click();
-
-      await page.waitFor(timeout);
-
-      // Close page.
-      page.close(); 
-    }
-
-    console.log("Done.");
+      console.log("Done.");
+    });
   }
-  catch (error){
+  catch (error) {
     console.log("Error");
     console.error(error);
   }
+
 });
+
+async function scrapeItems(browser: puppeteer.Browser) {
+  // Loop every ean code.
+  for (let ean of eanCodes) {
+    let pot = {
+      PartitionKey: entGen.String(ean),
+      RowKey: entGen.String(uuidv4()),
+      Date: entGen.DateTime(new Date()),
+      Ean: entGen.String(ean),
+      Success: entGen.Boolean(false),
+      Title: entGen.String(null),
+      Price: entGen.Double(null),
+      Quantity: entGen.Int32(null)
+    };
+
+    // Open a new page.
+    const page = await browser.newPage();
+    await page.goto('https://www.bol.com/nl/');
+
+    // Search for an EAN code.
+    console.log("Searching for: " + ean);
+    await page.focus('#searchfor');
+    await page.keyboard.type(ean);
+
+    var searchBtn = await page.$("[data-test='search-button']");
+    await searchBtn.click();
+
+    await page.waitFor(timeout);
+
+    // Log the current url.
+    var url = page.url();
+    console.log("Page at: " + url);
+
+    // Get the title
+    var titleElement = await page.$("[data-test='title']");
+    const title = await page.evaluate(el => el.innerText, titleElement);
+    pot.Title = entGen.String(title);
+    console.log("Title: " + title);
+
+    // Get the price
+    var priceElement = await page.$("[data-test='price']");
+    const price = await page.evaluate(el => el.innerText, priceElement);
+    let floatPrice = parseFloat(price.replace(/\n/g, '').replace(',', '.'));
+    pot.Price = entGen.Double(floatPrice);
+    console.log("Price: " + floatPrice);
+
+    // Hit the buy btn.
+    var buyBtn = await page.$(".btn--buy");
+    if (buyBtn) {
+      console.log("Product in basket.");
+      buyBtn.click();
+    } else {
+      console.log("Product not added to basket.");
+      pot.Success = entGen.Boolean(false);
+      pot.Quantity = entGen.Int32(0);
+      insertRecord(pot);
+      return;
+    }
+
+    await page.waitFor(timeout);
+
+    // Close the modal.
+    var modalClose = await page.$("[data-test='modal-window-close']");
+    modalClose.click();
+
+    await page.waitFor(timeout);
+
+
+    // Go to the basket.
+    var basketBtn = await page.$("[data-test='basket-button']");
+    basketBtn.click();
+    console.log("At basket");
+
+    await page.waitFor(timeout);
+
+    // Log the current url.
+    var url = page.url();
+    console.log("Page at: " + url);
+
+    // Check for the highest number in the quantity select.
+    let maxVal: number | undefined = await page.evaluate(() => {
+      let m: number = 0;
+
+      var select = document.querySelector('#tst_quantity_dropdown') as HTMLSelectElement;
+      for (var i = 0; i < select.options.length; i++) {
+        if (select.options[i].value === "meer") {
+          return undefined;
+        }
+
+        let intVal = parseInt(select.options[i].value);
+        if (intVal > m) {
+          m = intVal;
+        }
+      }
+      return m;
+    });
+
+    // If the max value is undefined then we can select 'meer'.
+    if (maxVal == undefined) {
+      console.log("Max val is undefined");
+
+      // Select meer.
+      await page.select('#tst_quantity_dropdown', 'meer');
+
+      console.log("Select more");
+
+      await page.evaluate(() => {
+        var element = document.querySelector('input[type=tel]') as HTMLInputElement;
+        element.value = '';
+        element.focus();
+      });
+
+      // Enter 500
+      await page.type('input[type=tel]', '500');
+
+      console.log("Set q to 500");
+
+      // Press ok.
+      const okBtn = await page.$('.js_quantity_overlay_ok');
+      await okBtn.click();
+
+      console.log("Click ok");
+
+      await page.waitFor(timeout);
+
+      // Query the available quantity.
+      var availableQ: string = await page.evaluate(() => {
+        var e = document.querySelector('#tst_quantity_dropdown') as HTMLSelectElement;
+        return e.options[e.selectedIndex].value;
+      });
+
+      console.log("Max value: " + availableQ);
+      pot.Quantity = entGen.Int32(parseInt(availableQ));
+
+    } else {
+      console.log("Max value: " + maxVal);
+      pot.Quantity = entGen.Int32(maxVal);
+    }
+
+    // Remove it from the basket.
+    const removeBtn = await page.$('#tst_remove_from_basket');
+    await removeBtn.click();
+
+    await page.waitFor(timeout);
+
+    // Close page.
+    page.close();
+
+    pot.Success = entGen.Boolean(true);
+
+    insertRecord(pot);
+  }
+}
+
+function insertRecord(pot: object) {
+  tableService.insertEntity('pointsintime', pot, async function (error: Error) {
+    if (!error) {
+      console.log("Entity inserted");
+    }
+  });
+}
